d26b9e260206ac0ead556f5e234811fb37b8f7a9
Hannah Manfredi <hmanfredi@gmail.com>
2020-12-14 17:36:05 -0800

    Refactor someInstance.pop so it returns popped off (last in) value and can now add and remove items sequentiallt.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,36 +1,31 @@
 //LIFO, added to top (end), removed from top (end), beginning is last out
 
+//allows sequentially additing and removing items
+
 var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-  // someInstance[storage] = storage;
   var count = 0;
 
   // Implement the methods below
 
   //add to stack
-  someInstance.push = function(value) { //{1:'a', 2:'b'}, count = 2
+  someInstance.push = function(value) {
     count ++;
     storage[count] = value;
-    // return storage;
   };
-  // console.log(someInstance.push('a'));
 
   //remove from stack
-  someInstance.pop = function() { //top = count - 1
-    //decrement count to access last in value
-
-    //delete last in value
-    var remove = storage[count];
-    delete remove;
-    console.log('make sure remove deleted: ', storage)
+  someInstance.pop = function() {
+    var removed = storage[count];
+    delete storage[count];
     count --;
-    // return storage;
+    return removed;
   };
 
-  //measure stack size (# of keys)
+  //measure stack size (number of keys)
   someInstance.size = function() {
     if (count <= 0) {
       return 0;
@@ -39,13 +34,6 @@ var Stack = function() {
     }
   };
 
-  // console.log('storage: ', storage);
-  // console.log('someInstance: ', someInstance);
-
   return someInstance;
 
 };
-
-var x = Stack();
-console.log(x);
-
