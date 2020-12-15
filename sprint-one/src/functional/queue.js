281f0e22c80e67ab1e64d96a7f585b5e8ce89994
Hannah Manfredi <hmanfredi@gmail.com>
2020-12-14 17:47:26 -0800

    Refactor someInstance.dequeue—generate keys array and whatever is 0-indexed was the first-in key. Use that key to access whatever value was stored there in the storage {}, then delete that key in storage and return the dequeued value

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,22 +5,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function(value) { //{1:'a', 2:'b', 3:'c'} [1, 2, 3]
+    count ++;
+    storage[count] = value;
   };
 
   someInstance.dequeue = function() {
+    var countKeys = Object.keys(storage);
+    console.log(countKeys);
+    var remove = countKeys[0]; //'a'
+    var dequeued = storage[remove];
+    delete storage[remove];
+    count --;
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
 };
-
-//O:
-//I:
-//C:
-//E:
