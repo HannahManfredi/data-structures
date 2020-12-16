04ae122c804dab64932d5c7987717ae47d936056
Hannah Manfredi <hmanfredi@gmail.com>
2020-12-16 08:58:03 -0800

    "Complete functional-shared stack and queue"

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,9 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var stackInstance = Object.create(stackMethods);
+
+  stackInstance.storage = {};
+  stackInstance.count = 0;
+
+  return stackInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  push: function() {
+
+  },
+
+  pop: function() {
+
+  },
+
+  size: function(){
+
+  }
+
+};
 
 
 // Do:
