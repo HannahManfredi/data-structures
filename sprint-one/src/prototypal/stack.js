b2cc967268f5a672986253e0ab248c106732cdc1
Hannah Manfredi <hmanfredi@gmail.com>
2020-12-16 09:15:33 -0800

    "Complete prototypal stack and queue"

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,32 +1,37 @@
+//LIFO
+
 var Stack = function() {
 
   var stackInstance = Object.create(stackMethods);
 
   stackInstance.storage = {};
   stackInstance.count = 0;
+  stackInstance.hasPopped = 0;
 
   return stackInstance;
 };
 
 var stackMethods = {
 
-  push: function() {
-
+  push: function(val) {
+    this.count ++;
+    this.storage[this.count] = val;
   },
 
   pop: function() {
-
+    var remove = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count --;
+    this.hasPopped ++;
+    return remove;
   },
 
-  size: function(){
-
+  size: function() {
+    if (this.count === 0 || this.hasPopped > this.count) {
+      return 0;
+    } else {
+      return this.count;
+    }
   }
 
 };
-
-
-// Do:
-// Work within the src/protoypal/ folder.
-// Use Object.create  with the object from step 2 to create instances of your class
-// Don't:
-// Use the keyword new
