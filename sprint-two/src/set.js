7b52bc5fc6ee5823dac9edad7384287af2d796b6
Hannah Manfredi <hmanfredi@gmail.com>
2021-01-07 18:32:03 -0800

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
+O(n)
 setPrototype.add = function(item) {
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
+O(n)
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
+O(n)
 setPrototype.remove = function(item) {
+  var idx = this._storage.indexOf(item);
+  this._storage.splice(idx, 1);
 };
 
 /*
